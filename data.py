69d67bc4de1150d049b7fbf9fd05820e66a8a11c
Renganath Chokkalingam <rengnath1234@gmail.com>
2026-03-31 15:17:53 +0530

    fix: Make data.py modular with get_batch fn

--- data.py
+++ data.py
@@ -19,10 +19,9 @@ n = int(0.9 * len(data))
 train_data = data[:n]
 val_data = data[n:]
 
-
-data_split = train_data
-ix = torch.randint(len(data_split) - block_size, (batch_size,))
-x = torch.stack([data_split[i:i+block_size] for i in ix])
-y = torch.stack([data_split[i+1:i+block_size+1] for i in ix])
-x.to(device)
-y.to(device)
+def get_batch(split):
+    data_split = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data_split) - block_size, (batch_size,))
+    x = torch.stack([data_split[i:i+block_size] for i in ix])
+    y = torch.stack([data_split[i+1:i+block_size+1] for i in ix])
+    return x.to(device), y.to(device)
